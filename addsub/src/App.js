6a90a39afd6c9bcc51cd85a004acd33a0e83dbc6
AniketBadadhe0570 <Dk0570@digikull.com>
2022-01-13 21:33:04 +0530

    add

---
+++ addsub/src/App.js
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Addsub from './Addsub';
+
+function App() {
+  return (
+    <div className="App">
+      <Addsub/>
+    </div>
+  );
+}
+
+export default App;
